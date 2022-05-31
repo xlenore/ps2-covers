21f8e14f6e66fa78eaa1ea99bca743340cea8a09
LouiseSulyvahn <57191159+LouiseSulyvahn@users.noreply.github.com>
2022-05-31 15:35:52 -0400

    1.2 Update

--- README.md
+++ README.md
@@ -7,16 +7,8 @@
 - Put those files in your PCSX2 Folder
 - Run
 
-
-## Contributing
-
-- Make sure the serial is not in [**missing_covers**](https://github.com/LouiseSulyvahn/PCSX2_Cover_Downloader/tree/main/missing_covers/)
-- Cover must be from the correct region
-- Dimensions 250x350
-- The size must not exceed 100 kb
-- Format .jpg
-
-
 ## Credits
 * pcsx2.net
 * psxdatacenter.com
+* imkira3
+* waifu2x
--- pcsx2_cover_downloader.py
+++ pcsx2_cover_downloader.py
@@ -1,4 +1,3 @@
-from logging import exception
 import re
 from time import sleep
 from urllib.error import HTTPError
@@ -8,54 +7,57 @@ import urllib.request
 import json
 import os, sys
 
-COVERS_URL = "https://github.com/LouiseSulyvahn/PCSX2_Cover_Downloader/raw/main/covers/"
+COVERS_URL = 'https://github.com/LouiseSulyvahn/PCSX2_Cover_Downloader/raw/main/covers/'
 
-def serial_list(): #Get game serial
+def path():
     if getattr(sys, 'frozen', False):
         path = os.path.dirname(os.path.realpath(sys.executable))
     elif __file__:
         path = os.path.dirname(__file__)
-    with open(f'{path}\cache\gamelist.cache', errors='ignore') as file:
+    return path
+        
+
+def serial_list():  # Get game serial
+    with open(f'{path()}\cache\gamelist.cache', errors='ignore') as file:
         regex = re.compile('(\w{4}-\d{5})').findall(file.read())
         serial_list = list(dict.fromkeys(regex))
-        print('[LOG]:',colored(f'Found {len(serial_list)} games','green'))
+        print('[LOG]:', colored(f'Found {len(serial_list)} games', 'green'))
         if len(serial_list) == 0:
-            print('[ERROR]:',colored(f'You have 0 games installed','yellow'))
+            print('[ERROR]:', colored(f'You have 0 games installed', 'yellow'))
             input()
-            quit()
+            quit()  
         return serial_list
 
-def serial_to_name(serial:str): #Get game name using serial
-    if getattr(sys, 'frozen', False):
-        path = os.path.dirname(os.path.realpath(sys.executable))
-    elif __file__:
-        path = os.path.dirname(__file__)    
-    with open(f'{path}\GameIndex.json', encoding='utf-8-sig') as file:
+
+def serial_to_name(serial:str):  # Get game name using serial
+    with open(f'{path()}\GameIndex.json', encoding='utf-8-sig') as file:
         try:
             json_data = json.loads(file.read())
-            name = json_data[serial]["name"]
+            name = json_data[serial]['name']
             return name
         except KeyError:
-            print('[ERROR]:',colored(f'{serial} Not found. Skipping...','yellow'))
+            print('[ERROR]:', colored(f'{serial} Not found. Skipping...', 'yellow'))
             return None
-             
-def download_covers(serial_list): #Download Covers
-        for i in range(len(serial_list)):
-            game_serial = serial_list[i]
-            game_name = serial_to_name(serial_list[i])
-            if game_name != None:
-                print('[LOG]:',colored(f'Downloading {game_serial} | {game_name} cover...','green'))
-                try:
-                    urllib.request.urlretrieve(f'{COVERS_URL}{game_serial}.jpg', f'covers/{game_name}.jpg')
-                    sleep(3)
-                except HTTPError:
-                    print('[ERROR]:',colored(f'{game_serial} | {game_name} Not found. Report it in GitHub please...','yellow'))
-                    
-                      
-def run():
-        download_covers(serial_list())
-        print('[LOG]:',colored(f'Done!','green'))
-        input()
 
-init()    
+
+def download_covers(serial_list:list):  # Download Covers
+    for i in range(len(serial_list)):
+        game_serial = serial_list[i]
+        game_name = serial_to_name(serial_list[i])
+        if game_name != None:
+            print('[LOG]:',colored(f'Downloading {game_serial} | {game_name} cover...', 'green'))
+            try:
+                urllib.request.urlretrieve(f'{COVERS_URL}{game_serial}.jpg', f'covers/{game_name}.jpg')
+                sleep(1)
+            except HTTPError:
+                print('[ERROR]:',colored(f'{game_serial} | {game_name} Not found. Report it in GitHub please...','yellow'))        
+
+
+def run():
+    download_covers(serial_list())
+    print('[LOG]:', colored(f'Done!', 'green'))
+    input()
+
+
+init()
 run()
