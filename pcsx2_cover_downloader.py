30f688fd463813d66a5e1359c662b1fb46c8ac1f
Florin9doi <Florin9doi@users.noreply.github.com>
2022-06-18 17:11:35 +0300

    Performance and stability improvements

--- pcsx2_cover_downloader.py
+++ pcsx2_cover_downloader.py
@@ -12,7 +12,7 @@
 ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀
-    
+
      NO COVERS?
 """
 
@@ -46,7 +46,7 @@ def check_version():
         if float(version.read().decode('utf-8').replace('\n','')) != VERSION:
             print('[LOG]:', colored(f'New update available!\n', 'green'))
     except:
-        pass     
+        pass
 
 
 def serial_list():  # Get game serial
@@ -57,58 +57,51 @@ def serial_list():  # Get game serial
         if len(serial_list) == 0:
             print('[ERROR]:', colored(f'You have 0 games installed', 'red'))
             input()
-            quit()  
+            quit()
         return serial_list
-    
+
+
+def name_list():  # Get game name
+    name_list = {}
+    with open(f'{path()}\/resources\GameIndex.yaml', encoding='utf-8-sig') as file:
+        for key, value in yaml.load(file, Loader=yaml.CBaseLoader).items():
+            name_list[key] = value["name"]
+    return name_list
+
 
 def existing_covers():
     covers = [w.replace('.jpg', '') for w in os.listdir(f'{path()}\covers')]
     return covers
-  
-  
-"""" #GameIndex JSON
-def serial_to_name(serial:str):  # Get game name using serial
-    with open(f'{path()}\GameIndex.json', encoding='utf-8-sig') as file:
-        try:
-            json_data = json.loads(file.read())
-            name = json_data[serial]['name']
-            return name
-        except KeyError:
-            print('[WARNING]:', colored(f'{serial} Not found. Skipping...', 'yellow'))
-            return None
-"""
 
 
-def serial_to_name(serial:str):  # Get game name using serial
-    with open(f'{path()}\/resources\GameIndex.yaml', encoding='utf-8-sig') as file:
-        try:
-            yaml_file = yaml.load(file.read(), Loader=yaml.CBaseLoader)
-            return yaml_file[serial]["name"]
-        except KeyError:
-            print('[WARNING]:', colored(f'{serial} Not found. Skipping...', 'yellow'))
-            return None
+def serial_to_name(name_list, serial:str):  # Get game name using serial
+    try:
+        return name_list[serial]
+    except KeyError:
+        print('[WARNING]:', colored(f'{serial} Not found. Skipping...', 'yellow'))
+        return None
 
 
-def download_covers(serial_list:list):  # Download Covers
+def download_covers(serial_list:list, name_list):  # Download Covers
     existing_cover = existing_covers()
     for i in range(len(serial_list)):
         game_serial = serial_list[i]
-        game_name = serial_to_name(serial_list[i])
+        game_name = serial_to_name(name_list, game_serial)
         if game_name != None:
             if game_serial not in existing_cover:
-                print('[LOG]:',colored(f'Downloading {game_serial} | {game_name} cover...', 'green'))
+                print('[LOG]:', colored(f'Downloading {game_serial} | {game_name} cover...', 'green'))
                 try:
                     urllib.request.urlretrieve(f'{COVERS_URL}{game_serial}.jpg', f'covers/{game_serial}.jpg')
                     sleep(1)
                 except HTTPError:
-                    print('[WARNING]',colored(f'{game_serial} | {game_name} Not found. Report it in GitHub please...','yellow'))
+                    print('[WARNING]:', colored(f'{game_serial} | {game_name} Not found. Report it in GitHub please...', 'yellow'))
             else:
-                print('[WARNING]:',colored(f'{game_serial} | {game_name} already exist in \covers. Skipping...','yellow'))                
+                print('[WARNING]:', colored(f'{game_serial} | {game_name} already exist in \covers. Skipping...', 'yellow'))
 
 
 def run():
     check_version()
-    download_covers(serial_list())
+    download_covers(serial_list(), name_list())
     print('[LOG]:', colored(f'Done!', 'green'))
     input()
 
